eb616e685a2567ee0c686b2693846354a4c9367e
Luka Mis <luka.mis@gmail.com>
2017-02-17 16:04:03 +0100

    App.js is ready to fetch some data from remote server.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,36 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      location : ''
+    }
+  };
+
+  changeLocation = (evt) => {
+    this.setState({
+      location: evt.target.value
+    });
+  };
+
+  fetchData = (evt) => {
+    evt.preventDefault();
+    console.log('fetch data for', this.state.location);
+  };
+
   render() {
     return (
       <div>
         <h1>Weather</h1>
-        <form>
+        <form onSubmit={this.fetchData}>
           <label> I want to know the weather for
-            <input type="text" placeholder={"City, Country"} />
+            <input 
+              type="text"
+              placeholder={"City, Country"}
+              value={this.state.location}
+              onChange={this.changeLocation}
+            />
           </label>
         </form>
       </div>  
